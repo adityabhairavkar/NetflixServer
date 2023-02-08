4050627dcae3cf311b03a3f4fd4759387dc24451
adib070 <74231226+adib070@users.noreply.github.com>
2023-02-08 14:58:31 +0530

    opentelemetryLogs:  OTLP logs added

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Net.NetworkInformation;
 using Microsoft.AspNetCore.Mvc;
 using NetflixServer.Models;
 
@@ -6,24 +7,72 @@ namespace NetflixServer.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+  private readonly ILogger<HomeController> _logger;
+  public HomeController(ILogger<HomeController> logger)
     {
-        _logger = logger;
-    }
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+  }
 
-    public IActionResult Index()
+  public IActionResult Index()
     {
-        return View();
+    _logger.LogInformation("Home view initiated");
+
+    return View();
     }
 
     public IActionResult Privacy()
     {
-        return View();
+    _logger.LogInformation("Privacy view initiated");
+
+    return View();
     }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public string TestConnection()
+    {
+      try
+      {
+        Ping myPing = new Ping();
+        PingReply reply = myPing.Send("www.google.com", 1000);
+        if (reply != null)
+        {
+         // Console.WriteLine("Status :  " + reply.Status + " \n Time : " + reply.RoundtripTime.ToString() + " \n Address : " + reply.Address);
+        }
+      Thread.Sleep(60);
+      _logger.LogInformation("Testing Connection:"+" Status :  " + reply.Status + " \n Time: " + reply.RoundtripTime.ToString() + " \n Address: " + reply.Address);
+
+      return "Status :  " + reply.Status + " \n Time : " + reply.RoundtripTime.ToString() + " \n Address : " + reply.Address;
+
+    }
+    catch(Exception e)
+      {
+      _logger.LogError("Testing Connection Failed:" + " Status :  " +e);
+
+      return "ERROR: You have Some TIMEOUT issue";
+      }
+    }
+  public string TestConnections()
+  {
+
+    try
+    {
+      Ping myPing = new Ping();
+      PingReply reply = myPing.Send("www.g10oogle.com", 1000);
+      if (reply != null)
+      {
+        // Console.WriteLine("Status :  " + reply.Status + " \n Time : " + reply.RoundtripTime.ToString() + " \n Address : " + reply.Address);
+      }
+      Thread.Sleep(60);
+      _logger.LogError("Testing Connection Failed:" + " Status :  " + reply.Status + " \n Time: " + reply.RoundtripTime.ToString() + " \n Address: " + reply.Address);
+
+      return "Status :  " + reply.Status + " \n Time : " + reply.RoundtripTime.ToString() + " \n Address : " + reply.Address;
+    }catch(Exception e)
+    {
+      _logger.LogError(e.StackTrace);
+      throw new Exception(e.StackTrace);
+    }
+
+  }
+  [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
--- Program.cs
+++ Program.cs
@@ -1,9 +1,48 @@
+using Chitragupta;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Resources;
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+Action<ResourceBuilder> configureResource = r => r.AddService(
+    serviceName: builder.Configuration.GetValue<string>("ServiceName") ?? AppDomain.CurrentDomain.FriendlyName,
+    serviceVersion: typeof(Program).Assembly.GetName().Version?.ToString() ?? "unknown",
+    serviceInstanceId: Environment.MachineName);
+builder.Logging.AddOpenTelemetry(options =>
+{
+  // Note: See appsettings.json Logging:OpenTelemetry section for configuration.
+  string logExporter = Environment.GetEnvironmentVariable("UseLogExporter") ?? "console";
+  string ep = Environment.GetEnvironmentVariable("OtlpEndpoint") ?? "http://localhost:4317";
+  Uri endpoint = new Uri(ep);
+  var resourceBuilder = ResourceBuilder.CreateDefault();
+  configureResource(resourceBuilder);
+  options.SetResourceBuilder(resourceBuilder);
 
+  switch (logExporter)
+  {
+    case "otlp":
+      options.AddOtlpExporter(otlpOptions =>
+      {
+        // Use IConfiguration directly for Otlp exporter endpoint option.
+        otlpOptions.Endpoint = endpoint;
+      });
+      break;
+    default:
+      options.AddConsoleExporter();
+      break;
+  }
+});
+
+
+//builder.Logging.AddEventLog();
 var app = builder.Build();
+
+// Configure LM Metrics 
+//MetricsCounter m = new MetricsCounter();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
