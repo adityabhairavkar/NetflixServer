2278591f99b37274151e3784a7c8f3300816c404
adib070 <74231226+adib070@users.noreply.github.com>
2023-02-08 13:46:36 +0530

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NetflixServer.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
